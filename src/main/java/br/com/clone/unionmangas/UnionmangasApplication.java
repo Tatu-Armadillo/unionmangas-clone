c8565b47c96102bb0f29c9adb00f5df4f2d868b1
Tatu-Armadillo <prohazor.fa@gmail.com>
2022-08-08 10:42:47 -0300

    init project

---
+++ src/main/java/br/com/clone/unionmangas/UnionmangasApplication.java
@@ -0,0 +1,13 @@
+package br.com.clone.unionmangas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnionmangasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnionmangasApplication.class, args);
+	}
+
+}
