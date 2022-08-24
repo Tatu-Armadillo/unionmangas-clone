dc98b78c9011a2cd725cb2f8e8346871e07019d3
Felipe Alves de Oliveira <prohazor.fa@gmail.com>
2022-08-24 14:29:38 -0300

    creating planned errors

---
+++ src/main/java/br/com/clone/unionmangas/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,29 @@
+package br.com.clone.unionmangas.exception.handler;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.com.clone.unionmangas.exception.ExceptionResponse;
+
+
+@ControllerAdvice
+@RestController
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionResponse> handleAllExceptions(Exception ex, WebRequest request) {
+        final ExceptionResponse exceptionResponse = new ExceptionResponse(
+                LocalDateTime.now(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
