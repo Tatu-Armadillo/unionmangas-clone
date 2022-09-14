0f476da68256068f9a541f62f60829605a0ac902
Felipe Alves de Oliveira <prohazor.fa@gmail.com>
2022-09-14 17:20:13 -0300

    update response MangaController

--- src/main/java/br/com/clone/unionmangas/controller/MangaController.java
+++ src/main/java/br/com/clone/unionmangas/controller/MangaController.java
@@ -1,9 +1,10 @@
 package br.com.clone.unionmangas.controller;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
@@ -22,6 +23,8 @@ import org.springframework.web.multipart.MultipartFile;
 import br.com.clone.unionmangas.dto.manga.MangaFindDto;
 import br.com.clone.unionmangas.dto.manga.MangaWeekDto;
 import br.com.clone.unionmangas.model.Manga;
+import br.com.clone.unionmangas.response.ResponseBase;
+import br.com.clone.unionmangas.response.ResponseBasePaginado;
 import br.com.clone.unionmangas.service.MangaService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -36,62 +39,69 @@ public class MangaController {
 
     @GetMapping
     @ApiOperation("endpoint responsible for fetching mangos by main and alias name")
-    public ResponseEntity<Page<MangaFindDto>> findAllMangas(
+    public ResponseEntity<ResponseBasePaginado<List<MangaFindDto>>> findAllMangas(
             @ApiIgnore @PageableDefault(sort = "mainTitle", direction = Direction.DESC) Pageable pageable,
             @ApiParam(name = "filter") @RequestParam(required = false, defaultValue = "") final String filter) {
-        var response = this.mangaService.findAllByName(filter, pageable);
-        return ResponseEntity.ok(response);
+        final var response = this.mangaService.findAllByName(filter, pageable);
+        final var base = ResponseBasePaginado.of(response);
+        return ResponseEntity.ok(base);
     }
 
     @GetMapping("/{idManga}")
     @ApiOperation("Endpoint responsible for searching the manga by id")
-    public ResponseEntity<Manga> findById(@ApiParam(name = "idManga") @PathVariable final Long idManga) {
-        var response = this.mangaService.findById(idManga);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<ResponseBase<Manga>> findById(@ApiParam(name = "idManga") @PathVariable final Long idManga) {
+        final var response = this.mangaService.findById(idManga);
+        final var base = ResponseBase.of(response);
+        return ResponseEntity.ok(base);
     }
 
     @GetMapping("/week")
     @ApiOperation("Endpoint responsible for returning the mangas updated in the week")
-    public ResponseEntity<Page<MangaWeekDto>> releaseWeek(
+    public ResponseEntity<ResponseBasePaginado<List<MangaWeekDto>>> releaseWeek(
             @ApiIgnore @PageableDefault(sort = "lastUpdate", direction = Direction.DESC) Pageable pageable) {
-        var response = this.mangaService.releaseWeek(pageable);
-        return ResponseEntity.ok(response);
+        final var response = this.mangaService.releaseWeek(pageable);
+        final var base = ResponseBasePaginado.of(response);
+        return ResponseEntity.ok(base);
     }
 
     @PostMapping
     @Transactional
     @ApiOperation("Endpoint responsible for creating a new manga with author and genres")
-    public ResponseEntity<Manga> createManga(@ApiParam(name = "manga") @RequestBody final Manga manga) {
-        var respose = this.mangaService.create(manga);
-        return ResponseEntity.ok(respose);
+    public ResponseEntity<ResponseBase<Manga>> createManga(@ApiParam(name = "manga") @RequestBody final Manga manga) {
+        final var response = this.mangaService.create(manga);
+        final var base = ResponseBase.of(response);
+        return ResponseEntity.ok(base);
     }
 
     @PutMapping("/{idManga}")
     @Transactional
     @ApiOperation("Endpoint responsible for changing the existing sleeve in the system")
-    public ResponseEntity<Manga> updateManga(
+    public ResponseEntity<ResponseBase<Manga>> updateManga(
             @ApiParam(name = "idManga") @PathVariable final Long idManga,
             @ApiParam(name = "manga") @RequestBody final Manga manga) {
-        var respose = this.mangaService.update(idManga, manga);
-        return ResponseEntity.ok(respose);
+        final var response = this.mangaService.update(idManga, manga);
+        final var base = ResponseBase.of(response);
+        return ResponseEntity.ok(base);
     }
 
     @PutMapping("/image/{idManga}")
     @Transactional
     @ApiOperation("Endpoint responsible for inserting chapter cover in the system")
-    public ResponseEntity<?> updateImageManga(
+    public ResponseEntity<ResponseBase<String>> updateImageManga(
             @ApiParam(name = "idManga") @PathVariable final Long idManga,
             @ApiParam(name = "image") @RequestParam final MultipartFile image) {
         this.mangaService.updateCover(idManga, image);
-        return ResponseEntity.ok("OK");
+        final var base = ResponseBase.of("ok");
+        return ResponseEntity.ok(base);
     }
 
     @DeleteMapping("/{idManga}")
     @Transactional
     @ApiOperation("Endpoint responsible for deleting mango from the system")
-    public ResponseEntity<?> delete(@ApiParam(name = "idManga") @PathVariable final Long idManga) {
+    public ResponseEntity<ResponseBase<String>> delete(@ApiParam(name = "idManga") @PathVariable final Long idManga) {
         var response = this.mangaService.delete(idManga);
-        return ResponseEntity.ok(response);
+        final var base = ResponseBase.of(response);
+        return ResponseEntity.ok(base);
     }
 
 }
