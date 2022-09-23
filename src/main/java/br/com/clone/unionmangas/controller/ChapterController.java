941306bb2aff2a9fd19e998dbd77b4eb47e41c5e
Felipe Alves de Oliveira <prohazor.fa@gmail.com>
2022-09-23 11:48:42 -0300

    docs endpoints for Chapter and Genre

--- src/main/java/br/com/clone/unionmangas/controller/ChapterController.java
+++ src/main/java/br/com/clone/unionmangas/controller/ChapterController.java
@@ -17,13 +17,20 @@ import br.com.clone.unionmangas.model.Chapter;
 import br.com.clone.unionmangas.service.ChapterService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
 
 @RestController
 @RequestMapping("/chapters")
+@Tag(name = "Chapter", description = "Endpoints for Managing Chapters")
 public class ChapterController {
 
     private final ChapterService chapterService;
-    
+
     @Autowired
     public ChapterController(ChapterService chapterService) {
         this.chapterService = chapterService;
@@ -31,6 +38,15 @@ public class ChapterController {
 
     @GetMapping("/{idManga}")
     @ApiOperation("Endpoint responsible for fetching the pages inside the manga")
+    @Operation(summary = "Pages inside the manga", description = "responsible for fetching the pages inside the manga", tags = {
+            "Chapter" }, responses = {
+                    @ApiResponse(description = "Success", responseCode = "200", content = {
+                            @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Chapter.class))) }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     public ResponseEntity<List<Chapter>> findChaptersByManga(
             @ApiParam(name = "idManga") @PathVariable Long idManga) {
         final var response = this.chapterService.findChaptersByManga(idManga);
@@ -40,9 +56,16 @@ public class ChapterController {
     @Transactional
     @PostMapping("/{idManga}")
     @ApiOperation("Endpoint responsible for adding pages to manga")
+    @Operation(summary = "Add pages", description = "adding pages to manga", tags = {
+            "Chapter" }, responses = {
+                    @ApiResponse(description = "Create", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Chapter.class))),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     public ResponseEntity<List<Chapter>> insertChapters(
-        @ApiParam(name = "idManga") @PathVariable final Long idManga,
-        @ApiParam(name = "chapter") @RequestBody final Chapter chapter) {
+            @ApiParam(name = "idManga") @PathVariable final Long idManga,
+            @ApiParam(name = "chapter") @RequestBody final Chapter chapter) {
         final var response = this.chapterService.insertChapters(idManga, chapter);
         return ResponseEntity.ok(response);
     }
--- src/main/java/br/com/clone/unionmangas/controller/GenreController.java
+++ src/main/java/br/com/clone/unionmangas/controller/GenreController.java
@@ -17,14 +17,21 @@ import br.com.clone.unionmangas.response.ResponseBasePaginado;
 import br.com.clone.unionmangas.service.GenreService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/genres")
+@Tag(name = "Genre", description = "Endpoints for Managing Genres")
 public class GenreController {
 
     private final GenreService genreService;
-    
+
     @Autowired
     public GenreController(GenreService genreService) {
         this.genreService = genreService;
@@ -32,8 +39,17 @@ public class GenreController {
 
     @GetMapping
     @ApiOperation("Endpoint responsible for showing existing genres in the system")
+    @Operation(summary = "Existing Genres", description = "responsible for showing existing genres in the system", tags = {
+            "Genre" }, responses = {
+                    @ApiResponse(description = "Success", responseCode = "200", content = {
+                            @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Genre.class))) }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            })
     public ResponseEntity<ResponseBasePaginado<List<Genre>>> getAllGenres(
-            @ApiIgnore @PageableDefault(sort = "name", direction = Direction.ASC) Pageable pageable, 
+            @ApiIgnore @PageableDefault(sort = "name", direction = Direction.ASC) Pageable pageable,
             @ApiParam(name = "filter") @RequestParam(required = false, defaultValue = "") final String name) {
         final var base = this.genreService.findAllByName(pageable, name);
         final var response = ResponseBasePaginado.of(base);
