94961f9ea00621ac901c0674c4c4a4a2d0a1b16f
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-05-23 14:44:51 -0300

    fix-access to documentation

--- src/main/java/br/com/clone/unionmangas/config/OpenApiConfig.java
+++ src/main/java/br/com/clone/unionmangas/config/OpenApiConfig.java
@@ -3,9 +3,11 @@ package br.com.clone.unionmangas.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class OpenApiConfig {
@@ -20,7 +22,12 @@ public class OpenApiConfig {
                         .termsOfService("https://github.com/Tatu-Armadillo")
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("https://github.com/Tatu-Armadillo")));
+                                .url("https://github.com/Tatu-Armadillo")))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer").bearerFormat("JWT")));
     }
 
 }
