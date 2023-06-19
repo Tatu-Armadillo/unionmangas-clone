84521e524ea7a2bac53baee7498c996e7cfe4df3
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-06-19 12:09:09 -0300

    fix - age AgeGroupEnum

--- src/main/java/br/com/clone/unionmangas/model/enumerator/AgeGroupEnum.java
+++ src/main/java/br/com/clone/unionmangas/model/enumerator/AgeGroupEnum.java
@@ -5,9 +5,9 @@ public enum AgeGroupEnum {
     LIVRE(0, "LIVRE", "Livre para todos os publicos"),
     MAIOR_10(10, "MAIOR_10", "Não recomendado para menores de 10 anos"),
     MAIOR_12(12, "MAIOR_12", "Não recomendado para menores de 12 anos"),
-    MAIOR_14(12, "MAIOR_14", "Não recomendado para menores de 14 anos"),
-    MAIOR_16(12, "MAIOR_16", "Não recomendado para menores de 16 anos"),
-    MAIOR_18(12, "MAIOR_18", "Não recomendado para menores de 18 anos");
+    MAIOR_14(14, "MAIOR_14", "Não recomendado para menores de 14 anos"),
+    MAIOR_16(16, "MAIOR_16", "Não recomendado para menores de 16 anos"),
+    MAIOR_18(18, "MAIOR_18", "Não recomendado para menores de 18 anos");
 
     private Integer age;
     private String chave;
