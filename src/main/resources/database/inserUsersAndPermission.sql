a02a4a9018bc071070546f62c6ef984d64f53e30
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-06-19 19:03:32 -0300

    update scripts sqls

--- src/main/resources/database/inserUsersAndPermission.sql
+++ src/main/resources/database/inserUsersAndPermission.sql
@@ -1,4 +1,4 @@
 insert into permission (description) values ('ADMIN'), ('SCAN_ADM'), ('COMMON_READER');
 INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-	('admin', 'Adminstrador Principal', 'c3a7d64cf6bcf3b5bb0870cd138bce7a2523632c804e4765c894eac2cf5d6402e35a5b01de2290c0', b'1', b'1', b'1', b'1');
+	('admin', 'Adminstrador Principal', '$2a$12$y.lFLRl9u0/Dk5jtBMRZLOYe0uiKVYqPOX32bin.Rm.WvsFmEIncW', b'1', b'1', b'1', b'1');
 INSERT INTO `user_permission` (`id_user`, `id_permission`) VALUES (1, 1);
