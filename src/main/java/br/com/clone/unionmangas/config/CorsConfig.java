bed1fe22c7a7bd555c47409531a79272435124ba
Tatu-Armadillo <69278300+Tatu-Armadillo@users.noreply.github.com>
2023-06-19 19:38:20 -0300

    Update CorsConfig.java

--- src/main/java/br/com/clone/unionmangas/config/CorsConfig.java
+++ src/main/java/br/com/clone/unionmangas/config/CorsConfig.java
@@ -8,11 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        final var httpValid = "http://localhost:3000/,http://localhost:8080/,https://www.google.com/";
-        final String[] allowedOrigins = httpValid.split(",");
+        // final var httpValid = "http://localhost:4200/,http://localhost:8080/,https://www.google.com/";
+        // final String[] allowedOrigins = httpValid.split(",");
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins(allowedOrigins)
+                .allowedOrigins("/**")
                 .allowCredentials(true);
     }
 }
